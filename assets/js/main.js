518f86c397ded1fe3b5a359da7e5db3aa37032a7
manhctd20 <cmanh0603@gmail.com>
2025-07-16 16:02:25 +0700

    update: 16/07

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,36 @@
 // Load header & footer
 fetch("components/header.html")
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
 
+fetch("components/certificate.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("certificate-section").innerHTML = data;
+    new Swiper(".certificate-swiper", {
+      loop: true,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: ".certificate-next",
+        prevEl: ".certificate-prev",
+      },
+      breakpoints: {
+        0: { slidesPerView: 2.5 },
+        768: { slidesPerView: 2.5 },
+      },
+    });
+  });
+
+fetch("components/subscribe.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("subscribe-section").innerHTML = data;
+  });
+
 fetch("components/footer.html")
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     document.getElementById("footer").innerHTML = data;
   });
